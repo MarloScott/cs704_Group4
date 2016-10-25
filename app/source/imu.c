8b6b116a65cd78e27b2136c9d865df85d6ec0cbe
esay286 <esay286@aucklanduni.ac.nz>
2016-10-25 15:14:26 +1300

    bug

--- app/source/imu.c
+++ app/source/imu.c
@@ -8,28 +8,6 @@
 #include "../../modules/libmpu9250/lib/mpu9250/mpu9250.h"
 #include "imu_adaptor.h"
 
-uint32_t SquareRoot(uint32_t a_nInput)
-{
-    uint32_t op  = a_nInput;
-    uint32_t res = 0;
-    uint32_t one = 1uL << 30; // The second-to-top bit is set: use 1u << 14 for uint16_t type; use 1uL<<30 for uint32_t type
-
-    // "one" starts at the highest power of four <= than the argument.
-    while (one > op) {
-        one >>= 2;
-    }
-
-    while (one != 0) {
-        if (op >= res + one) {
-            op = op - (res + one);
-            res = res +  2 * one;
-        }
-        res >>= 1;
-        one >>= 2;
-    }
-    return res;
-}
-
 void IMU_DEVICE_INIT(struct IMU_DEVICE_POSE *devicePose, struct mpu9250_s device)
 {
   devicePose->device = device;
